b7502b6990ec24d9f8358952a8e297b34ca7dafd
Yannick Essoulou Gadimi <yannickessoulougadimi@Yannicks-MacBook-Pro.local>
2024-12-18 23:15:26 -0500

    update tags, new vars for bucket naming

--- modules/alias/main.tf
+++ modules/alias/main.tf
@@ -1,9 +1,7 @@
-# Retrieve information about your hosted zone from AWS
 data "aws_route53_zone" "created" {
   name = var.domain_name
 }
 
-# Create an alias that will point to the cloudfront distribution domain name
 resource "aws_route53_record" "alias" {
   zone_id                  = data.aws_route53_zone.created.zone_id
   name                     = var.domain_name
